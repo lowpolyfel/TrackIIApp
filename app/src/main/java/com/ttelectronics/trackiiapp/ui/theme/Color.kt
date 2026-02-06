12c38c11e2539b853b18dc6c012cf73b4906ba68
hayfel <hayfeldev@gmail.com>
2026-02-06 12:35:44 -0700

    Add TrackII UI flow screens

--- app/src/main/java/com/ttelectronics/trackiiapp/ui/theme/Color.kt
+++ app/src/main/java/com/ttelectronics/trackiiapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.ttelectronics.trackiiapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TTBlue = Color(0xFF32A7DF)
+val TTBlueDark = Color(0xFF1E7FB2)
+val TTBlueLight = Color(0xFFBFE9FB)
+val TTBlueTint = Color(0xFFEAF7FF)
+val TTAccent = Color(0xFF5FD1FF)
+val TTTextPrimary = Color(0xFF0C2233)
+val TTTextSecondary = Color(0xFF345569)
